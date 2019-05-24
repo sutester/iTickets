89b13b76b7e0ced5d5c668cb2a9dd4249e25cc91
Danny <kedanlin8121@gmail.com>
2019-05-24 19:47:23 +0800

    add function for submit order and get unpaid order list.

--- common/work.py
+++ common/work.py
@@ -28,7 +28,7 @@ def get_departments_list():
         "Referer":
         r"https://huaxi2.mobimedical.cn/index.php?g=Wap&m=WxView&d=registerAndAppoint&a=index",
         "Cookie":
-        "PHPSESSID=bd94eag0mllub549a5ugqi9u07"
+        "PHPSESSID=qra14valb8b4c7r4oq08l265g4"
     }
     params = {"g": "WapApi", "m": "Register", "a": "dutyDeptList", "ts": "0"}
     response = S.get(Host, headers=header, params=params, verify=False).json()
@@ -105,7 +105,7 @@ def get_card_list():
         "Referer":
         r"https://huaxi2.mobimedical.cn/index.php?g=Wap&m=WxView&d=registerAndAppoint&a=index",
         "Cookie":
-        "PHPSESSID=bd94eag0mllub549a5ugqi9u07"
+        "PHPSESSID=qra14valb8b4c7r4oq08l265g4"
     }
     params = {"g": "WapApi", "m": "Card", "a": "cardList"}
     response = S.get(Host, headers=header, params=params, verify=False).json()
@@ -170,6 +170,57 @@ def create_verify():
     #return response.text
 
 
+def submit_order():
+    header = {
+        "Accept":
+            "*/*",
+        "Origin":
+            "https://huaxi2.mobimedical.cn",
+        "X-Requested-With":
+            "XMLHttpRequest",
+        "User-Agent":
+            r"Mozilla/5.0 (Linux; Android 9; ONEPLUS A6010 Build/PKQ1.180716.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 MQQBrowser/6.2 TBS/044607 Mobile Safari/537.36 MMWEBID/9309 MicroMessenger/7.0.3.1400(0x2700033C) Process/tools NetType/WIFI Language/zh_CN",
+        "Referer":
+            r"https://huaxi2.mobimedical.cn/index.php?g=Wap&m=WxView&d=registerAndAppoint&a=index",
+        "Cookie":
+            "PHPSESSID=qra14valb8b4c7r4oq08l265g4"
+    }
+    params = {"g": "WapApi", "m": "Register", "a": "submitReg"}
+    data = {
+        "tagArray": "90,496",
+        "schedulid": "39789",
+        "userid": "2201256",
+        "is_ai": "",
+        "token":"wx155869757230528"
+    }
+    response = S.post(
+        Host, headers=header, params=params, data=data, verify=False).json()
+    assert response.get('state') == 1
+    assert response.get('errorMsg') == '成功'
+    return response
+
+def get_unpaid_list():
+    header = {
+        "Accept":
+            "*/*",
+        "Origin":
+            "https://huaxi2.mobimedical.cn",
+        "X-Requested-With":
+            "XMLHttpRequest",
+        "User-Agent":
+            r"Mozilla/5.0 (Linux; Android 9; ONEPLUS A6010 Build/PKQ1.180716.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 MQQBrowser/6.2 TBS/044607 Mobile Safari/537.36 MMWEBID/9309 MicroMessenger/7.0.3.1400(0x2700033C) Process/tools NetType/WIFI Language/zh_CN",
+        "Referer":
+            r"https://huaxi2.mobimedical.cn/index.php?g=Wap&m=WxView&d=registerAndAppoint&a=index",
+        "Cookie":
+            "PHPSESSID=qra14valb8b4c7r4oq08l265g4"
+    }
+    params = {"g": "WapApi", "m": "OrderApi", "a": "waitPayList"}
+    response = S.get(
+        Host, headers=header, params=params, verify=False).json()
+    assert response.get('state') == 1
+    assert response.get('errorMsg') == '成功'
+    return response
+
 if __name__ == '__main__':
     import io, sys
     sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding='utf-8')
@@ -180,3 +231,4 @@ if __name__ == '__main__':
     print(get_card_list())
     # print(get_reg_queue_start())
     #print(create_verify())
+    print(get_unpaid_list())
