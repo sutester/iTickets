050d22722c11674cd27ecc62d8b3e6f9d0e8a616
Danny <Dannyd.ke@arrowasia.com>
2019-06-03 10:42:49 +0800

    debug

--- helper/BD_Identifier.py
+++ helper/BD_Identifier.py
@@ -5,43 +5,61 @@ captcha identify
 #Define access token. once the token been issued it would be validated for 30 mins.
 accessToken = '24.8603abe537c4061b66b3dbe1875299f0.2592000.1561554145.282335-16148227'
 
+
 class captchaIdentifier():
-    def __init__(self,s):
+    def __init__(self, s):
         self.api_url = r'https://aip.baidubce.com/rest/2.0/ocr/v1/accurate_basic'
         self.S = s
-        self.headers = {'Content-Type':'application/x-www-form-urlencoded'}
-
+        self.headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
     def getText(self, img):
-        params = {"access_token":accessToken}
+        params = {"access_token": accessToken}
         data = {"image": img}
-        r = self.S.post(url=self.api_url,params= params,data=data,headers=self.headers,verify=False).json()
-        return r.get('words_result')[0].get('words') if r.get('words_result_num') > 0 else None
+        r = self.S.post(
+            url=self.api_url,
+            params=params,
+            data=data,
+            headers=self.headers,
+            verify=False).json()
+        return r
+        # return r.get('words_result')[0].get(
+        #     'words') if r.get('words_result_num') > 0 else None
 
 
 class objIdentifier():
-    def __init__(self,s):
+    def __init__(self, s):
         self.S = s
         self.api_url = r'https://aip.baidubce.com/rest/2.0/image-classify/v2/advanced_general'
         self.headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    
-    def getText(self,img):
+
+    def getText(self, img):
         data = {"image": img}
-        params = {"access_token":accessToken}
-        r = self.S.post(url=self.api_url,params= params,data=data,headers=self.headers,verify=False).json()
-        return r.get('result')[0].get('keyword') if r.get('result_num') > 0 else None
+        params = {"access_token": accessToken}
+        r = self.S.post(
+            url=self.api_url,
+            params=params,
+            data=data,
+            headers=self.headers,
+            verify=False).json()
+        return r.get('result')[0].get(
+            'keyword') if r.get('result_num') > 0 else None
 
 
 if __name__ == '__main__':
-    FilePath = r'../images/'
+    FilePath = r'./images/'
     from requests import session
     s = session()
     import base64
+    import io, sys
+    sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding='utf-8')
+    sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding='utf-8')
+
     def get_image(file_path):
         with open(file_path, 'rb') as f:
             return base64.b64encode(f.read())
-    ci = captchaIdentifier(s)
-    print(ci.getText(get_image(FilePath+'test_question.jpg')))
 
-    oid = objIdentifier(s)
-    print(oid.getText(get_image(FilePath+'banana.jpg')))
+    ci = captchaIdentifier(s)
+    print(ci.getText(get_image(FilePath + 'test1.png')))
+
+    # oid = objIdentifier(s)
+    # print(oid.getText(get_image(FilePath+'banana.jpg')))
--- helper/image_analysis.py
+++ helper/image_analysis.py
@@ -10,17 +10,18 @@ from .BD_Identifier import captchaIdentifier
 from .BD_Identifier import objIdentifier
 import base64
 
+
 class ImageAnalysis:
-    def __init__(self,session):
+    def __init__(self, session):
         self.session = session
         self.ci = captchaIdentifier(self.session)
         self.oi = objIdentifier(self.session)
 
-    def get_image(self,file_path):
-        with open(file_path,'rb') as f:
+    def get_image(self, file_path):
+        with open(file_path, 'rb') as f:
             return base64.b64encode(f.read())
 
-    def get_question_title(self,file_path):
+    def get_question_title(self, file_path):
         '''
         To get the text of the question
         :param file_path:
@@ -28,7 +29,7 @@ class ImageAnalysis:
         '''
         return self.ci.getText(self.get_image(file_path))
 
-    def get_question_type(self,question):
+    def get_question_type(self, question):
         if '计算' in question:
             return 1
         elif '对应' in question:
@@ -36,27 +37,28 @@ class ImageAnalysis:
         else:
             return 0
 
-    def get_answer(self,question):
-        question = question.replace('?','')
+    def get_answer(self, question):
+        question = question.replace('?', '')
         if self.get_question_type(question):
             if self.get_question_type(question) == 1:
                 question = question.split('结果')[1]
                 if '加' in question:
-                    m,n = question.split('加')
+                    m, n = question.split('加')
                     return int(m) + int(n)
                 elif '减' in question:
-                    m,n = question.split('减')
+                    m, n = question.split('减')
                     return int(m) - int(n)
                 elif '乘' in question:
-                    m,n = question.split('乘')
+                    m, n = question.split('乘')
                     return int(m) * int(n)
                 elif '除' in question:
-                    m,n = question.split('除')
+                    m, n = question.split('除')
                     return int(m) / int(n)
                 else:
                     raise SystemError
             else:
                 question = question.split('图片')[1]
+
 
 if __name__ == '__main__':
     FilePath = r'../images/'
